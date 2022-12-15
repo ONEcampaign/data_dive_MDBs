89a37f4958513527a5d6f95d1d59ebb12a2bb666
Jorge Rivera <jorge.rivera@one.org>
2022-12-15 11:11:26 +0100

    updates

--- scripts/full_update.py
+++ scripts/full_update.py
@@ -1,6 +1,8 @@
 from scripts.intel_tracker.heatmap_tracker import heatmap_data
+from scripts.world_bank_votes.download_shares import votes_chart_data
 from scripts.logger import logger
 
 if __name__ == "__main__":
-    logger.info("Triggered heatmap update")
+    logger.info("Triggered full update")
     heatmap_data()
+    votes_chart_data()
